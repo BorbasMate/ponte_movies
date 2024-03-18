c2131c9dc016f5516410d807868147c9aa524a6c
BorbasMate <borbas.mategabor76@gmail.com>
2024-03-18 18:47:09 +0100

    add MovieControllerTest and MovieServiceTest

---
+++ src/test/java/bmg/hu/ponte_movies/controller/MovieControllerTest.java
@@ -0,0 +1,133 @@
+package bmg.hu.ponte_movies.controller;
+
+import bmg.hu.ponte_movies.dto.MovieListItem;
+import bmg.hu.ponte_movies.dto.RatingListItem;
+import bmg.hu.ponte_movies.exception.GlobalExceptionHandler;
+import bmg.hu.ponte_movies.service.MovieService;
+import bmg.hu.ponte_movies.service.RatingService;
+import bmg.hu.ponte_movies.validator.RatingCreationCommandValidator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.MessageSource;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@ExtendWith(MockitoExtension.class)
+class MovieControllerTest {
+
+    private MockMvc mockMvc;
+    @Mock
+    private MovieService movieServiceMock;
+
+
+    @BeforeEach
+    public void setUp() {
+        MovieController movieController =
+                new MovieController(movieServiceMock);
+
+        mockMvc = MockMvcBuilders.standaloneSetup(movieController)
+                .setControllerAdvice(new GlobalExceptionHandler(messageSource()))
+                .build();
+    }
+
+    @AfterEach
+    public void validate() {
+        validateMockitoUsage();
+    }
+
+    @Test
+    void testGetMovies() throws Exception {
+        //given
+        MovieListItem item1 = new MovieListItem();
+        item1.setMovieId(100L);
+        item1.setTitle("Star Wars IV");
+        item1.setReleaseDate("1980-01-01");
+        item1.setOverView("A new hope");
+        item1.setOriginalLanguage("en");
+        item1.setPosterPath("/xxx.jpg");
+        item1.setCast(List.of("Mark Hamill", "Harrison Ford"));
+        item1.setProductionCompanies(List.of("20 Century fox", "Visage studio"));
+        item1.setImages(List.of("/aaa.jpg", "/bbb.jpg"));
+
+        MovieListItem item2 = new MovieListItem();
+        item2.setMovieId(200L);
+        item2.setTitle("Star Wars VII");
+        item2.setReleaseDate("2010-01-01");
+        item2.setOverView("The force awakens");
+        item2.setOriginalLanguage("en");
+        item2.setPosterPath("/yyy.jpg");
+        item2.setCast(List.of("Adam Driver", "Daisy Ridley"));
+        item2.setProductionCompanies(List.of("Paramount", "Bad Robot"));
+        item2.setImages(List.of("/ccc.jpg", "/ddd.jpg"));
+
+
+        when(movieServiceMock.findMovies()).thenReturn(List.of(item1, item2));
+
+        //when
+        this.mockMvc.perform(get("/api/movies"))
+
+                //then
+                .andExpect(status().is(200))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].movieId", is(100)))
+                .andExpect(jsonPath("$[0].title", is("Star Wars IV")))
+                .andExpect(jsonPath("$[0].releaseDate", is("1980-01-01")))
+                .andExpect(jsonPath("$[0].overView", is("A new hope")))
+                .andExpect(jsonPath("$[0].originalLanguage", is("en")))
+                .andExpect(jsonPath("$[0].posterPath", is("/xxx.jpg")))
+                .andExpect(jsonPath("$[0].cast", hasSize(2)))
+                .andExpect(jsonPath("$[0].productionCompanies", hasSize(2)))
+                .andExpect(jsonPath("$[0].images", hasSize(2)))
+                .andExpect(jsonPath("$[1].movieId", is(200)))
+                .andExpect(jsonPath("$[1].title", is("Star Wars VII")))
+                .andExpect(jsonPath("$[1].releaseDate", is("2010-01-01")))
+                .andExpect(jsonPath("$[1].overView", is("The force awakens")))
+                .andExpect(jsonPath("$[1].originalLanguage", is("en")))
+                .andExpect(jsonPath("$[1].posterPath", is("/yyy.jpg")))
+                .andExpect(jsonPath("$[1].cast", hasSize(2)))
+                .andExpect(jsonPath("$[1].productionCompanies", hasSize(2)))
+                .andExpect(jsonPath("$[1].images", hasSize(2)));
+
+
+        verify(movieServiceMock, times(1)).findMovies();
+        verifyNoMoreInteractions(movieServiceMock);
+
+    }
+
+
+    private MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+
+        messageSource.setBasename("messages");
+        messageSource.setUseCodeAsDefaultMessage(true);
+
+        return messageSource;
+    }
+
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
---
+++ src/test/java/bmg/hu/ponte_movies/service/MovieServiceTest.java
@@ -0,0 +1,102 @@
+package bmg.hu.ponte_movies.service;
+
+import bmg.hu.ponte_movies.component.TmdbService;
+import bmg.hu.ponte_movies.dto.MovieListItem;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration
+class MovieServiceTest {
+
+    private MovieService movieService;
+
+    @Mock
+    private TmdbService tmdbServiceMock;
+
+
+    @BeforeEach
+    public void setup() {
+        movieService = new MovieService(tmdbServiceMock);
+    }
+
+    @AfterEach
+    public void validate() {
+        validateMockitoUsage();
+    }
+
+    @Test
+    void testFindMovies() {
+        //given
+        MovieListItem item1 = new MovieListItem();
+        item1.setMovieId(100L);
+        item1.setTitle("Star Wars IV");
+        item1.setReleaseDate("1980-01-01");
+        item1.setOverView("A new hope");
+        item1.setPosterPath("/xxx.jpg");
+
+        MovieListItem item2 = new MovieListItem();
+        item2.setMovieId(200L);
+        item2.setTitle("Star Wars VII");
+        item2.setReleaseDate("2010-01-01");
+        item2.setOverView("The force awakens");
+        item2.setPosterPath("/yyy.jpg");
+
+        when(tmdbServiceMock.getTopRatedMovies()).thenReturn(List.of(item1, item2));
+
+        item1.setCast(List.of("Mark Hamill", "Harrison Ford"));
+        item2.setCast(List.of("Adam Driver", "Daisy Ridley"));
+
+        doNothing().when(tmdbServiceMock).getActorsForMovie(any(MovieListItem.class));
+
+        item1.setProductionCompanies(List.of("20 Century fox", "Visage studio"));
+        item2.setProductionCompanies(List.of("Paramount", "Bad Robot"));
+
+        doNothing().when(tmdbServiceMock).getProductionCompaniesForMovie(any(MovieListItem.class));
+
+        item1.setImages(List.of("/aaa.jpg", "/bbb.jpg"));
+        item2.setImages(List.of("/ccc.jpg", "/ddd.jpg"));
+
+        doNothing().when(tmdbServiceMock).getImagesForMovie(any(MovieListItem.class));
+
+
+        //when
+        List<MovieListItem> movieListItems = movieService.findMovies();
+
+        //then
+        assertEquals(2, movieListItems.size());
+        assertEquals("Star Wars IV", movieListItems.get(0).getTitle());
+        assertEquals("Star Wars VII", movieListItems.get(1).getTitle());
+        assertFalse(movieListItems.get(0).getCast().isEmpty());
+        assertFalse(movieListItems.get(1).getCast().isEmpty());
+        assertFalse(movieListItems.get(0).getProductionCompanies().isEmpty());
+        assertFalse(movieListItems.get(1).getProductionCompanies().isEmpty());
+        assertFalse(movieListItems.get(0).getImages().isEmpty());
+        assertFalse(movieListItems.get(1).getImages().isEmpty());
+
+
+        verify(tmdbServiceMock, times(1)).getTopRatedMovies();
+        verify(tmdbServiceMock, times(2)).getActorsForMovie(any(MovieListItem.class));
+        verify(tmdbServiceMock, times(2)).getProductionCompaniesForMovie(any(MovieListItem.class));
+        verify(tmdbServiceMock, times(2)).getImagesForMovie(any(MovieListItem.class));
+        verifyNoMoreInteractions(tmdbServiceMock);
+
+    }
+
+
+
+}
