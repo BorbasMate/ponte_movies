8c5f3402373dac2217b679be76a13651b611d453
BorbasMate <borbas.mategabor76@gmail.com>
2024-03-18 12:00:46 +0100

    add MovieListItem initial dto

---
+++ src/main/java/bmg/hu/ponte_movies/dto/MovieListItem.java
@@ -0,0 +1,13 @@
+package bmg.hu.ponte_movies.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class MovieListItem {
+
+    private String title;
+    private String releaseDate;
+
+}
